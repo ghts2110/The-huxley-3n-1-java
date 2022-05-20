3a9d93f78ee48d6c0799b83227c441d2dc1dc5b5
G4br13l-T0rr3s <59540600+ghts2110@users.noreply.github.com>
2022-05-20 19:32:56 -0300

    Update Main.java
    O problema dessa questão é entender a logica . A logica é, pegar os numeros entre i e j, depois printar  o numero com maior ciclo.

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
+/*Considere o seguinte algoritmo para gerar uma sequência de números. Comece com um inteiro n. Se n for par, divida por 2.
+Se n for ímpar, multiplique por 3 e some 1. Repita esse processo com o novo valor de n, terminando quando n = 1.
+Por exemplo, a seguinte sequência de números será gerada quando n é 22:
+22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+Embora ainda não exista uma prova, os matemáticos acreditam que esse algoritmo sempre termina com n=1, para qualquer inteiro n.
+Bem, para este problema aqui no Huxley, essa propriedade se mantém para qualquer inteiro menor que 1.000.000.
+Para uma entrada n, o "tamanho do ciclo" de n é a quantidade de números gerados até o 1 (incluindo o próprio 1).
+No exemplo acima, o "tamanho do ciclo" de 22 é 16. 
+Dado dois números i e j, determine o máximo "tamanho do ciclo" dentre todos os números entre i e j, incluindo tanto i quanto j. */
+
 import java.util.Scanner;
 
 public class thehuxley{
